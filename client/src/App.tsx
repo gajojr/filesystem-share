a18b1f761511f9344a92a1e95f618a2cc2b4c65e
Andrija Gajic <andrijagajicbusiness@gmail.com>
2021-11-11 10:51:32 +0100

    feat: add home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from 'react-router-dom';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage.page'));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const fallbackComponent = () => <div>Loading...</div>;
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element=
+                    {
+                        <Suspense fallback={fallbackComponent()}>
+                            <HomePage />
+                        </Suspense>
+                    }
+                >
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ client/src/pages/HomePage/HomePage.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HomePageContainer } from './HomePage.styles';
+
+const HomePage = () => {
+    return (
+        <HomePageContainer>
+            Home page
+        </HomePageContainer>
+    )
+}
+
+export default HomePage;
